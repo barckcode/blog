ed5564a569162dfeec98e4b81b40bb9fe52834e0
BarckCode <barckcode@gmail.com>
2021-05-08 14:53:03 +0200

    Updated script deploy - v1.4

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -9,28 +9,29 @@ USR_BINARY="/usr/bin"
 
 # Paths
 SOURCE_CODE="/var/www/helmcode.com"
+LOG_PATH="/tmp/deploy.log"
 
 # Commands
 DATE="$(date):"
 
-
+######################### SCRIPT
+echo "*********************************************" >> $LOG_PATH
+echo $DATE >> $LOG_PATH
 cd $SOURCE_CODE
-$USR_BINARY/git checkout .
-$USR_BINARY/git pull
+$USR_BINARY/git checkout . >> $LOG_PATH
+$USR_BINARY/git pull >> $LOG_PATH
 
 if [[ $? -eq 0 ]]
 then
-    echo "************************" >> /tmp/deploy.log
-    echo "$DATE Pull ejecutado con éxito" >> /tmp/deploy.log
-    $USR_BINARY/docker restart flask_app
+    echo "$DATE Pull ejecutado con éxito" >> $LOG_PATH
+    $USR_BINARY/docker restart flask_app >> $LOG_PATH
 
     if [[ $? -eq 0 ]]
     then
-        echo "$DATE Restart de flask_app ejecutado con éxito" >> /tmp/deploy.log
+        echo "$DATE Restart de flask_app ejecutado con éxito" >> $LOG_PATH
     else
-        echo "$DATE ERROR - Restart de flask_app ejecutado sin éxito" >> /tmp/deploy.log
+        echo "$DATE ERROR - Restart de flask_app ejecutado sin éxito" >> $LOG_PATH
     fi
 else
-    echo "************************" >> /tmp/deploy.log
-    echo "$DATE ERROR - Pull ejecutado sin éxito" >> /tmp/deploy.log
+    echo "$DATE ERROR - Pull ejecutado sin éxito" >> $LOG_PATH
 fi
