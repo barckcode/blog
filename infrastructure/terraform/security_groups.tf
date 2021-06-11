3c604a30a2ea3679961b1258424fac4de947fb75
BarckCode <barckcode@gmail.com>
2021-06-11 20:12:54 +0200

    Added Sauron server

--- infrastructure/terraform/security_groups.tf
+++ infrastructure/terraform/security_groups.tf
@@ -1,6 +1,44 @@
 #
 # Segurity Groups
 ##
+# Sauron
+resource "aws_security_group" "sauron_sg" {
+  name        = "sauron_sg"
+  description = "Allow inbound traffic"
+  vpc_id      = aws_vpc.main.id
+
+  ingress {
+    description = "SSH from all"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "HTTP"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "HTTPS"
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+
+  tags = {
+    Name     = "sauron_sg"
+    Creation = "terraform"
+  }
+}
+
+
 # WEB
 resource "aws_security_group" "web_sg" {
   name        = "web_sg"
