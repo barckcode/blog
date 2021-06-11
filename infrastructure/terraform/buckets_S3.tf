8ff478c23963f4c061de7846b8a99d4249acce44
BarckCode <barckcode@gmail.com>
2021-06-11 19:17:15 +0200

    Added new S3 bucket

--- infrastructure/terraform/buckets_S3.tf
+++ infrastructure/terraform/buckets_S3.tf
@@ -7,3 +7,13 @@ resource "aws_s3_bucket" "static_files" {
     Creation    = "terraform"
   }
 }
+
+resource "aws_s3_bucket" "source_code" {
+  bucket = "source.helmcode.com"
+  acl    = "private"
+
+  tags = {
+    Name        = "source_code"
+    Creation    = "terraform"
+  }
+}
