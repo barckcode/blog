ba695a9ccc10b8ab7a6d6028d39e609545cc2b1b
BarckCode <barckcode@gmail.com>
2021-06-16 19:59:16 +0200

    Fixed SG of sauron

--- infrastructure/terraform/sauron.tf
+++ infrastructure/terraform/sauron.tf
@@ -37,6 +37,6 @@ resource "aws_eip" "sauron_public_ip" {
 
 # Attached Segurity Groups
 resource "aws_network_interface_sg_attachment" "sauron_sg_attachment" {
-  security_group_id    = aws_security_group.web_sg.id
+  security_group_id    = aws_security_group.sauron_sg.id
   network_interface_id = aws_instance.sauron.primary_network_interface_id
 }
