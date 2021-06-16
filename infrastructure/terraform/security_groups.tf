6ed0155cbbd8bcff3ecc113a08a5fa7fe4c45f8e
BarckCode <barckcode@gmail.com>
2021-06-16 20:25:16 +0200

    Added new SG for Jenkins

--- infrastructure/terraform/security_groups.tf
+++ infrastructure/terraform/security_groups.tf
@@ -24,6 +24,14 @@ resource "aws_security_group" "sauron_sg" {
   }
 
   ingress {
+    description = "Jenkins"
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
     description = "HTTPS"
     from_port   = 443
     to_port     = 443
